ff6941ba1369bf40415ab4806949fba566ac42bd
Diego Estrada <diegoe@ciencias.unam.mx>
2025-08-21 02:46:58 -0600

    Comenta codigo problematico

--- supabase/functions/schedule-feeding/index.ts
+++ supabase/functions/schedule-feeding/index.ts
@@ -3,6 +3,8 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
+//
+/*
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
