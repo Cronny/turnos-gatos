86019adcfa6042fb759f1fa24c0e467c88c10934
Diego Estrada <diegoe@ciencias.unam.mx>
2025-08-24 22:24:18 -0600

    Agrega tabla de feeding_periods a script de SQL

--- create_tables.sql
+++ create_tables.sql
@@ -13,6 +13,14 @@ CREATE TABLE feeding_schedule (
     user_id int references users(id) on delete cascade
 );
 
+CREATE TABLE feeding_periods (
+    id serial primary key,
+    user_id int references users(id) on delete cascade
+    start_date date not null unique,
+    end_date date not null unique,
+    compensated boolean default true -- Si al regresar se debe "pagar" los dias
+);
+
 -- Insercion de usuarios
 INSERT INTO users (name)
 VALUES 
